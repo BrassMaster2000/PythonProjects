b68e137e1c8b652277647b01aa6b64d9081612e5
TheWebRage <DavidWeber1@Weber.edu>
2018-11-13 19:04:31 -0700

    More print examples

--- print.py
+++ print.py
@@ -7,3 +7,19 @@ print("\n")
 
 firstName = "David"
 lastName = "Weber"
+
+print("Your name is " + firstName + " " + lastName + ".")
+
+print("{0} first name {1} last name".format(firstName, lastName))
+
+firstNumber = 5
+secondNumber = 10
+
+print(firstNumber + secondNumber)
+print(firstNumber, secondNumber)
+print("{0} is greater than {1}".format(secondNumber, firstNumber))
+
+highTestScore = 0.9372
+lowTestScore = 0.4598
+
+print("\nThe high score was " + str(highTestScore) + "\nThe low score was " + str(lowTestScore))
